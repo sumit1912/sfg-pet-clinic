919814ab7b1f80c10d33b5f240bbdae342f2d2dc
uia77569 <sumit.2.kumar@continental-corporation.com>
2019-11-03 20:11:44 +0530

    Committing specialities model

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,9 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Vet extends Person{
+import java.util.Set;
+
+public class Vet extends Person {
+
+    private Set<Speciality> specialities;
 
 }
